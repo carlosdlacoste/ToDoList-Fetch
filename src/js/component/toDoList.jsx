0cec21eb1dca3222cf41f6d489593d6bec648b3b
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-05-07 23:48:22 -0400

    Sincronizando lista cada vez que se agrega o elimina una tarea, metodo get no esta funcionando

--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -1,24 +1,92 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 
 const ToDoList = () => {
     
     const [inputValue, setInputValue] = useState("");
-    const [taskList, setTaskList] = useState([
-        {
-            description: "Estudiar Fetch",
-            status: true
-        }
-    ]);
-    const [itemCount, setItemCount] = useState(1);
+    const [taskList, setTaskList] = useState([]);
+    const [itemCount, setItemCount] = useState(0);
+
+    useEffect(() => {
+
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/carlosdlacoste', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp => {
+            console.log(resp.ok);
+            console.log(resp.status);
+            console.log(resp.text())
+            return resp.json();
+        })
+        .then(data => {
+            console.log(data);
+            setTaskList(data)  // revisar nuevamente esta linea de codigo
+
+            // data.map((element, index) => (
+                            
+            //     <li key={index} className="list-group-item d-flex justify-content-between">{element.label} {" "}
+            //         <button type="button" onClick={() => handleDeleteTask(index)} className="btn-close" aria-label="Close"></button>
+            //     </li>
+        
+            // ))  
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
+    useEffect(() => {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/carlosdlacoste', {
+            method: "PUT",
+            body: JSON.stringify(taskList),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp => {
+            console.log(resp.ok); 
+            console.log(resp.status); 
+            console.log(resp.text()); 
+            return resp.json(); 
+        })
+        .then(data => {
+            console.log(data); 
+            // setTaskList((taskList) => [...taskList, data]);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, [taskList])
 
     const handleAddTask = (event) => {
         setInputValue(event.target.value);
         if(event.key == "Enter" && inputValue != ""){
             setTaskList([...taskList, {
-                description: inputValue,
-                status: false
+                label: inputValue,
+                done: false
             }]);
+            // fetch('https://assets.breatheco.de/apis/fake/todos/user/carlosdlacoste', {
+            //     method: "POST",
+            //     body: JSON.stringify(taskList),
+            //     headers: {
+            //         "Content-Type": "application/json"
+            //     }
+            // })
+            // .then(resp => {
+            //     console.log(resp.ok); 
+            //     console.log(resp.status); 
+            //     console.log(resp.text()); 
+            //     return resp.json(); 
+            // })
+            // .then(data => {
+            //     console.log(data); 
+            // })
+            // .catch(error => {
+            //     console.log(error);
+            // });
             event.target.value = "";
             setInputValue(event.target.value);
             setItemCount(itemCount + 1);
@@ -32,6 +100,7 @@ const ToDoList = () => {
         );
         setItemCount(itemCount - 1);
     }
+
     
     return (
         <>
@@ -47,7 +116,7 @@ const ToDoList = () => {
                     {
                         taskList.length != 0 && taskList.map((element, index) => (
                             
-                                <li key={index} className="list-group-item d-flex justify-content-between">{element.description} {" "}
+                                <li key={index} className="list-group-item d-flex justify-content-between">{element.label} {" "}
                                     <button type="button" onClick={() => handleDeleteTask(index)} className="btn-close" aria-label="Close"></button>
                                 </li>
                         
