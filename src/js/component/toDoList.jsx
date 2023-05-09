27e2da65d3189f34ac1268ac8787f9fab4147719
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-05-09 13:01:14 -0400

    Reestructurado parte del codigo y decorado con bootstrap y css

--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -1,69 +1,77 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const ToDoList = () => {
-    
+
+    const [userNotFound, setUserNotFound] = useState(false)
+    const [username, setUsername] = useState('carlosdlacoste')
     const [inputValue, setInputValue] = useState("");
     const [taskList, setTaskList] = useState([]);
     const [itemCount, setItemCount] = useState(0);
 
-    useEffect(() => {
-
-        fetch('https://assets.breatheco.de/apis/fake/todos/user/carlosdlacoste', {
+    const loadTasks = () => {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
             }
         })
-        .then(resp => {
-            console.log(resp.ok);
-            console.log(resp.status);
-            // console.log(resp.text())
-            return resp.json();
-        })
-        .then(data => {
-            console.log(data);
-            setTaskList(data);
-            setItemCount(data.length);
-
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }, [])
+            .then(resp => {
+                if(resp.ok){
+                    setUserNotFound(true)
+                }
+                console.log(resp.status);
+                // console.log(resp.text())
+                return resp.json();
+            })
+            .then(data => {
+                console.log(data);
+                setTaskList(data);
+                setItemCount(data.length);
+    
+            })
+            .catch(err => {
+                setUserNotFound(false)
+            })
+    }
 
     useEffect(() => {
-        fetch('https://assets.breatheco.de/apis/fake/todos/user/carlosdlacoste', {
-            method: "PUT",
-            body: JSON.stringify(taskList),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(resp => {
-            console.log(resp.ok); 
-            console.log(resp.status); 
-            // console.log(resp.text()); 
-            return resp.json(); 
-        })
-        .then(data => {
-            console.log(data); 
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }, [taskList])
+        loadTasks()
+    }, [])
 
     const handleAddTask = (event) => {
-        setInputValue(event.target.value);
-        if(event.key == "Enter" && inputValue != ""){
-            setTaskList([...taskList, {
-                label: inputValue,
-                done: false
-            }]);
-            event.target.value = "";
-            setInputValue(event.target.value);
-            setItemCount(itemCount + 1);
+
+        if (event.key == "Enter" && inputValue != "") {
+            fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+                method: "PUT",
+                body: JSON.stringify(taskList),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(resp => {
+                    console.log(resp.ok);
+                    console.log(resp.status);
+                    // console.log(resp.text());
+                    if (resp.ok) {
+                        setTaskList([...taskList, {
+                            label: inputValue,
+                            done: false
+                        }]);
+                        event.target.value = "";
+                        setInputValue(event.target.value);
+                        setItemCount(itemCount + 1);
+                    }
+                    return resp.json();
+                })
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }else{
+            setInputValue(event.target.value)
         }
     }
 
@@ -77,76 +85,88 @@ const ToDoList = () => {
 
     const deleteAllTasks = () => {
 
-        fetch('https://assets.breatheco.de/apis/fake/todos/user/carlosdlacoste', {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
             method: "DELETE",
             headers: {
                 "Content-Type": "application/json"
             }
         })
+            .then(resp => {
+                console.log(resp.ok);
+                if(resp.ok){
+                    setUserNotFound(false) 
+                }
+                console.log(resp.status);
+                return resp.json();
+            })
+            .then(data => {
+                console.log(data);
+
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    const createUser = () => {
+
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+            method: "POST",
+            body: JSON.stringify([]), 
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })  
         .then(resp => {
-            console.log(resp.ok);
-            console.log(resp.status);
+            if(resp.ok){
+                loadTasks()
+                // setUserNotFound(true)
+            }
             return resp.json();
         })
         .then(data => {
             console.log(data);
-            // setTaskList(data);
-            // setItemCount(data.length);
 
         })
         .catch(err => {
             console.log(err);
         })
-
-
-        // fetch('https://assets.breatheco.de/apis/fake/todos/user/carlosdlacoste', {
-        //     method: "POST",
-        //     body: [],
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     }
-        // })
-        // .then(resp => {
-        //     console.log(resp.ok);
-        //     console.log(resp.status);
-        //     return resp.json();
-        // })
-        // .then(data => {
-        //     console.log(data);
-
-        // })
-        // .catch(err => {
-        //     console.log(err);
-        // })
-
     }
 
-    
+
     return (
         <>
             <div className='container'>
-                <h1 className='text-center'>ToDo List with Fetch</h1>
-                <div className="input-group mb-3">
-                    <input type="text" onKeyUp={handleAddTask} className="form-control" placeholder="What do you need to do?" aria-label="Username" aria-describedby="basic-addon1"/>
+                <h1 className='text-center text-success'>ToDo List with Fetch 📋</h1>
+                {!userNotFound && 
+                    <div className='d-flex flex-column justify-content-center my-3'>
+                        <h4>Insert an username:</h4>
+                        <input type="text" onChange={(evento) => setUsername(evento.target.value)} value={username} placeholder="nombre de usuario" className="form-control mb-2" />
+                        <button onClick={() => createUser()} className='btn btn-primary' >Create User</button>
+                    </div>
+                }
+                {  userNotFound && <>
+                <div className="input-group my-3">
+                    <input type="text" onKeyUp={handleAddTask} className="form-control" placeholder="What do you need to do?" aria-label="Username" aria-describedby="basic-addon1" />
                 </div>
                 <ul className="list-group">
-                    {
+                    {/* {
                         taskList.length == 0 && <li className="list-group-item">No hay tareas pendientes</li>
+                    } */}
+                    {   taskList && taskList.map((element, index) => (
+
+                            <li key={index} className="list-group-item d-flex justify-content-between">{element.label} {" "}
+                                <button type="button" onClick={() => handleDeleteTask(index)} className="btn-close" aria-label="Close"></button>
+                            </li>
+
+                        ))
                     }
                     {
-                        taskList.length != 0 && taskList.map((element, index) => (
-                            
-                                <li key={index} className="list-group-item d-flex justify-content-between">{element.label} {" "}
-                                    <button type="button" onClick={() => handleDeleteTask(index)} className="btn-close" aria-label="Close"></button>
-                                </li>
-                        
-                        ))  
-                    }
-                    {
-                        taskList.length != 0 && <li className="list-group-item">{itemCount}{" "}item left</li>
+                        taskList.length != 0 && <li className="list-group-item bg-dark text-light">{itemCount}{" "}item left</li>
                     }
                 </ul>
-                <button type="button" onClick={() => deleteAllTasks()} className="btn btn-danger mt-4" style={{width: "100%"}}>Delete all tasks</button>
+                <button type="button" onClick={() => deleteAllTasks()} className="btn btn-danger mt-4" style={{ width: "100%" }}>Delete all tasks</button>
+                </>}
             </div>
         </>
     );
